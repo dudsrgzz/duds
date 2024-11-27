1e5ceba7d6301763c6ce4e604eac2a8f75f133f9
dudsrgzz <168751228+dudsrgzz@users.noreply.github.com>
2024-11-27 09:04:49 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Tecnologia',
+    'Para que serve o Python na ciência de dados?',
+    'Python é amplamente utilizado em ciência de dados para análise, visualização e aprendizado de máquina.'
+)
+
+criaCartao(
+    'Geopolítica',
+    'Qual país tem maior influência na Europa atualmente?',
+    'Atualmente, a Alemanha é considerada um dos países mais influentes na Europa.'
+)
+
+criaCartao(
+    'Desenvolvimento de Software',
+    'Como as funções facilitam o trabalho em equipe?',
+    'Funções permitem dividir o trabalho, facilitando a colaboração e manutenção do código.'
+)
+
+criaCartao(
+    'Cultura',
+    'Quais são algumas saudações formais em inglês?',
+    'Saudações formais em inglês incluem "Good morning", "Good afternoon" e "Good evening".'
+)
